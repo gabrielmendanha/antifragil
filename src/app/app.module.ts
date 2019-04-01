93b2f366b2253a737e5bed4d395d18fc9ad4f490
Gabriel Mendanha <gabrielmendanha@hotmail.com>
2019-04-01 00:18:05 -0300

    Tela de login, favicon, token de autenticacao via facebook, scss inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  FacebookLoginProvider
+} from "angular-6-social-login";
+
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig([
+    {
+      id: FacebookLoginProvider.PROVIDER_ID,
+      provider: new FacebookLoginProvider("767147586986883")
+    }
+  ]);
+  return config;
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SocialLoginModule,
+    AngularFontAwesomeModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, AfterContentInit } from "@angular/core";
+import { AuthService, FacebookLoginProvider } from "angular-6-social-login";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent implements OnInit {
+  socialPlatformProvider: any = FacebookLoginProvider.PROVIDER_ID;
+
+  constructor(private socialAuthService: AuthService) {}
+
+  ngOnInit() {}
+
+  public socialSignIn() {
+    this.socialAuthService
+      .signIn(this.socialPlatformProvider)
+      .then(userData => {
+        console.log(userData);
+      });
+  }
+}
